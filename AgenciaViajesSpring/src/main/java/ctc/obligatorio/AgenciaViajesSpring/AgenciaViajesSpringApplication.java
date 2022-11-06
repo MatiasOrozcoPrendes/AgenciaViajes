8d1c531b5464261d5e29666ea392770be5d18575
MatiasOrozcoPrendes <matias.orozco.prendes@gmail.com>
2022-11-06 15:01:44 -0300

    Primer comit con los proyectos Expo y Spring en blanco.

---
+++ AgenciaViajesSpring/src/main/java/ctc/obligatorio/AgenciaViajesSpring/AgenciaViajesSpringApplication.java
@@ -0,0 +1,13 @@
+package ctc.obligatorio.AgenciaViajesSpring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AgenciaViajesSpringApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AgenciaViajesSpringApplication.class, args);
+	}
+
+}
---
+++ AgenciaViajesSpring/src/test/java/ctc/obligatorio/AgenciaViajesSpring/AgenciaViajesSpringApplicationTests.java
@@ -0,0 +1,13 @@
+package ctc.obligatorio.AgenciaViajesSpring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AgenciaViajesSpringApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
